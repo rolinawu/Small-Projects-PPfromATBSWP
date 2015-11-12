a5da94a9d0fbd3b4ab3dfc6a64f1fb1766ef821f
Rolina Wu <rolinawu79@gmail.com>
2015-11-11 23:27:43 -0500

    Add collatz

---
+++ collatz.py
@@ -0,0 +1,20 @@
+## (collatz num)takes number: num.
+## if number is even, then collatz() print num//2
+## else print 3*num +
+## keep going till num = 1
+
+def collatz(num):
+	while (num > 1):
+		if num % 2 == 0:
+			print num / 2
+		else:
+			print 3 * num + 1
+	return num
+
+def main():
+	entry = int(raw_input("Number?"))
+	collatz(entry)
+
+
+if __name__ == '__main__':
+	main()
